90cfbdc664762187f42894664dbd8745770c2b77
Edward Castillo <castilloedwarda@gmail.com>
2020-02-20 12:22:22 -0500

    Added home stuff

---
+++ src/components/Battle.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import Phaser from 'phaser'
+import { IonPhaser } from '@ion-phaser/react'
+
+export default class Battle extends Component {
+
+    render() {
+        return
+    }
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,67 @@
 import React, {Component} from 'react'
 import Phaser from 'phaser'
 import { IonPhaser } from '@ion-phaser/react'
+import PickPoke from './PickPoke'
+import Profile from './Profile'
+import Battle from './Battle'
 
 export default class Home extends Component {
-    state = {
-        initialize: false,
-        game: {
-          width: "100%",
-          height: "100%",
-          type: Phaser.AUTO,
-          scene: {}
-        }
-      }
-      render() {
-        const { initialize, game } = this.state
-        return (
-          <IonPhaser game={game} initialize={initialize} />
-        )
-      }
+  state = {
+    conditionalRender: ""
+  }
+  
+  renderHome = () => {
+    this.setState({
+      conditionalRender: ""
+    })
+  }
+
+  renderPickPoke = () => {
+    this.setState({
+      conditionalRender: "pickpoke"
+    })
+  }
+
+  renderProfile = () => {
+    this.setState({
+      conditionalRender: "profile"
+    })
+  }
+
+  renderBattle = () => {
+    this.setState({
+      conditionalRender: "battle"
+    })
+  }
+
+  render() {
+      return (
+          <div>
+            <h1>Pokemon Battle Simulator</h1>
+            {this.state.conditionalRender === "" ? <button onClick={this.renderPickPoke}>Pick Pokemon</button> :null}
+            {this.state.conditionalRender === "pickpoke" ? <PickPoke /> : null}
+            {this.state.conditionalRender === "" ? <button onClick={this.renderProfile}>Log In</button> :null}
+            {this.state.conditionalRender === "profile" ? <Profile renderHome={this.renderHome}/> : null}
+            {this.state.conditionalRender === "" ? <button onClick={this.renderBattle}>Randomized Battle</button> :null}
+            {this.state.conditionalRender === "battle" ? <Battle /> : null}
+          </div>
+      )
+  }
 }
 
+// state = {
+//     initialize: false,
+//     game: {
+//       width: "100%",
+//       height: "100%",
+//       type: Phaser.AUTO,
+//       scene: {}
+//     }
+//   }
+//   render() {
+//     const { initialize, game } = this.state
+//     return (
+//       <IonPhaser game={game} initialize={initialize} />
+//     )
+//   }
 
-
-        // state = {
-    
-        // }
-    
-        // render() {
-        //     return (
-        //         <h1>hi</h1>
-        //     )
-        // }
